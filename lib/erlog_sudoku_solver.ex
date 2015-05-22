51c48d604b6519ee1841658a762450f8719d5d58
Wojtek Gawroński <afronski@gmail.com>
2015-05-22 11:09:19 +0200

    Adding comment about missing integration.

--- lib/erlog_sudoku_solver.ex
+++ lib/erlog_sudoku_solver.ex
@@ -3,6 +3,7 @@ defmodule ErlogSudokuSolver do
     {:ok, start} = :erlog.new
     {:ok, solver} = :erlog.consult("./lib/sudoku.pl", start)
 
+    # *TODO*: How to invoke `L = [ [_,4,2,_], [_,2,1,_], [4,_,_,2], [2,_,_,1] ], sudoku(L).` and receive back filled `L`?
     case :erlog.prove({:append, [1], [2], { :solution }}, solver) do
       {{:succeed, [ solution: solution ]}, _state} -> solution
       :fail -> :no_solution
